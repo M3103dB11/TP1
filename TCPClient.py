5ed4455723497fd655e433e0914a783af028b8ff
M3103dB11 <auber006@gmail.com>
2017-03-08 14:55:36 +0100

    Add files via upload

--- TCPClient.py
+++ TCPClient.py
@@ -1,10 +1,19 @@
 from socket import *
-serverName = 'localhost'
+serverName = '10.98.2.96'
+serverName2 = '10.98.2.90'
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-sentence = raw_input('Input lowercase sentence:')
-clientSocket.send(sentence)
-modifiedSentence = clientSocket.recv(1024)
-print 'From Server:', modifiedSentence
-clientSocket.close()
+while 1:
+	clientSocket = socket(AF_INET, SOCK_STREAM)
+	clientSocket.connect((serverName,serverPort))
+	sentence = raw_input('Input lowercase sentence:')
+	clientSocket.send(sentence)
+	modifiedSentence = clientSocket.recv(1024)
+	#print 'From Server:', modifiedSentence
+	clientSocket.close()
+	clientSocket = socket(AF_INET, SOCK_STREAM)
+	clientSocket.connect((serverName2,serverPort))
+	sentence = raw_input('Input server aurelien:')
+	clientSocket.send(sentence)
+	modifiedSentence = clientSocket.recv(1024)
+	#print 'From Server aurelien :', modifiedSentence
+	clientSocket.close()
